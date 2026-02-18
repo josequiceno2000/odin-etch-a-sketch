f2e66af490283ab086e16f9e1106812d775bdb0c
Jose Quiceno <josequiceno000@gmail.com>
2026-02-17 21:11:17 -0600

    Center and color grid

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 const container = document.querySelector("#container");
+let containerTotalWidth = container.offsetWidth;
 
 for (let i = 0; i < 16 ** 2; i++) {
     let squareDiv = document.createElement("div");
-    squareDiv.style.width = "6px";
-    squareDiv.style.height = "6px";
-    squareDiv.style.backgroundColor = "orange";
-    squareDiv.style.border = "1px solid green";
+    squareDiv.style.width = "6.25%";
+    squareDiv.style.height = "6.25%";
+    squareDiv.style.backgroundColor = "lightblue";
+    squareDiv.style.border = "1px solid #ccc";
     squareDiv.classList.add("squareDiv");
     container.appendChild(squareDiv);
 }
