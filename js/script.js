5171a053024de1981d056e4c4a5f4b5b119d8fa8
Jose Quiceno <josequiceno000@gmail.com>
2026-02-17 21:06:19 -0600

    Display square grid

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ const container = document.querySelector("#container");
 
 for (let i = 0; i < 16 ** 2; i++) {
     let squareDiv = document.createElement("div");
-    squareDiv.style.width = "2px";
-    squareDiv.style.height = "2px";
+    squareDiv.style.width = "6px";
+    squareDiv.style.height = "6px";
     squareDiv.style.backgroundColor = "orange";
     squareDiv.style.border = "1px solid green";
     squareDiv.classList.add("squareDiv");
